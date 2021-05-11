d9d292e149fd098f8650e11ac9ad63fe13f44bcb
Your Name <catcoder.php@gmail.com>
2021-05-11 14:19:34 -0500

    Validators in request

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,10 +2,6 @@
 
 
 namespace Catcoderphp\CustomConfigProvider;
-use Catcoderphp\CustomConfigProvider\Factory\Service\RequestHandlerServiceFactory;
-use Catcoderphp\CustomConfigProvider\Factory\Service\ResponseHandlerServiceFactory;
-use Catcoderphp\CustomConfigProvider\Service\RequestHandlerService;
-use Catcoderphp\CustomConfigProvider\Service\ResponseHandlerService;
 
 class ConfigProvider
 {
@@ -28,11 +24,9 @@ class ConfigProvider
      */
     public function getDependencyConfig()
     {
+        $configProviderCollection = new ConfigProviderCollection();
         return [
-            'factories' => [
-                ResponseHandlerService::class => ResponseHandlerServiceFactory::class,
-                RequestHandlerService::class => RequestHandlerServiceFactory::class
-            ],
+            'factories' => $configProviderCollection->getDependencies()
         ];
     }
 }
