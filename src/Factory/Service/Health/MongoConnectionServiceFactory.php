e08de02678f9ce2b793b1bd6cdb231a5e6454559
Hugo Hernández Valdez <hugo.hernandez@claroshop.com>
2021-05-26 17:52:39 -0500

    FIX: MongoConnectionService method for manual config

--- src/Factory/Service/Health/MongoConnectionServiceFactory.php
+++ src/Factory/Service/Health/MongoConnectionServiceFactory.php
@@ -15,17 +15,10 @@ class MongoConnectionServiceFactory
      */
     public function __invoke(ContainerInterface $container): MongoConnectionService
     {
-        $mongoConf = $container->get('config');
-        if (isset($mongoConf['mongo'])){
-            return new MongoConnectionService($mongoConf['mongo']);
-        }
-
         if ($container->has('Doctrine\ODM\MongoDB\DocumentManager')) {
             return new MongoConnectionService($container->get('Doctrine\ODM\MongoDB\DocumentManager'));
         }
 
-        throw new Exception(
-            'You do not have a previous configuration to be able to create an instance of MongoConnectionService'
-        );
+        return new MongoConnectionService();
     }
 }
--- src/Service/Health/MongoConnectionService.php
+++ src/Service/Health/MongoConnectionService.php
@@ -4,9 +4,6 @@ namespace Catcoderphp\CustomConfigProvider\Service\Health;
 
 use Exception;
 use MongoDB\Driver\Command;
-use MongoDB\Driver\Exception\ConnectionTimeoutException;
-use MongoDB\Driver\Exception\InvalidArgumentException;
-use MongoDB\Driver\Exception\RuntimeException;
 use MongoDB\Driver\Manager;
 
 class MongoConnectionService implements ConnectionServiceInterface
@@ -18,52 +15,97 @@ class MongoConnectionService implements ConnectionServiceInterface
      */
     private $mongo;
 
-    public function __construct($mongoConf)
+    private $config;
+
+    /**
+     * MongoConnectionService constructor.
+     * @param $mongoConf
+     * @throws Exception
+     */
+    public function __construct($mongoConf = null)
     {
-        if (is_object($mongoConf) || is_array($mongoConf)) {
+        $this->mongo = null;
+        $this->config = [];
+        if ($mongoConf != null) {
             if (is_object($mongoConf) && get_class($mongoConf) != 'Doctrine\ODM\MongoDB\DocumentManager') {
                 throw new Exception(
                     'The supplied argument is not valid.'
                 );
             }
-        }else{
+            $this->mongo = $mongoConf;
+        }
+    }
+
+    /**
+     * @param array $config
+     * @throws Exception
+     */
+    public function setDriverConfig(array $config)
+    {
+        if (!is_array($config)){
             throw new Exception(
                 'The supplied argument is not valid.'
             );
         }
-        $this->mongo = $mongoConf;
+
+        if (!isset($config['user'], $config['password'], $config['server'], $config['port'], $config['dbname'])) {
+            throw new Exception(
+                'The supplied array is not valid. '
+                . 'It should contain the following values user, password, server, port, dbname.'
+            );
+        }
+
+        $this->config = $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDriverConfig(): array
+    {
+        return $this->config;
     }
 
     /**
      * @return bool
-     * @throws ConnectionTimeoutException | RuntimeException | InvalidArgumentException | Exception
+     * @throws \MongoDB\Driver\Exception\Exception
+     * @throws Exception
      */
-    public function driverConnection(): bool
+    private function driverConnection(): bool
     {
-        if ($this->mongo == null){
-            return false;
+        if (empty($this->config)) {
+            throw new Exception(
+                'The supplied config is not valid. '
+                . 'It should contain the following values user, password, server, port, dbname.'
+            );
         }
 
         $url = "mongodb://%s:%s@%s:%s";
         $manager = new Manager(sprintf(
             $url,
-            $this->mongo['user'],
-            $this->mongo['password'],
-            $this->mongo['server'],
-            $this->mongo['port']
+            $this->config['user'],
+            $this->config['password'],
+            $this->config['server'],
+            $this->config['port']
         ));
 
         $command = new Command(['ping' => 1]);
-        $cursor = $manager->executeCommand($this->mongo['dbname'], $command)->toArray();
+        $cursor = $manager->executeCommand($this->config['dbname'], $command)->toArray();
         return !empty($cursor);
     }
 
     /**
      * @return bool
-     * @throws Exception
+     * @throws Exception|\MongoDB\Driver\Exception\Exception
      */
     private function odmConnection(): bool
     {
+        if ($this->mongo == null) {
+            throw new Exception(
+                'A DocumentManager was not provided. '
+                . 'You could use send a configuration to method setDriverConfigdbname. '
+            );
+        }
         $db = $this->mongo->getConfiguration()->getDefaultDB();
         $command = new Command(['ping' => 1]);
         $cursor = $this->mongo->getClient()->getManager()->executeCommand($db, $command)->toArray();
@@ -72,12 +114,12 @@ class MongoConnectionService implements ConnectionServiceInterface
 
     /**
      * @return bool
-     * @throws Exception
+     * @throws Exception|\MongoDB\Driver\Exception\Exception
      */
     public function checkConnection(): bool
     {
         try {
-            if (is_array($this->mongo)) {
+            if (is_array($this->config)) {
                 return $this->driverConnection();
             }
             return $this->odmConnection();
