8e51a48a181154c3d13983fe97b5267c5ebd4d6a
Hugo Hernández Valdez <hugo.hernandez@claroshop.com>
2021-05-26 14:52:25 -0500

    FIX: ConfigProvider & Factory

--- src/ConfigProviderCollection.php
+++ src/ConfigProviderCollection.php
@@ -7,9 +7,17 @@ namespace Catcoderphp\CustomConfigProvider;
 use Catcoderphp\CustomConfigProvider\Factory\Mapper\MapperFactory;
 use Catcoderphp\CustomConfigProvider\Factory\Service\RequestHandlerServiceFactory;
 use Catcoderphp\CustomConfigProvider\Factory\Service\ResponseHandlerServiceFactory;
+use Catcoderphp\CustomConfigProvider\Factory\Service\Health\EndpointConnectionServiceFactory;
+use Catcoderphp\CustomConfigProvider\Factory\Service\Health\MongoConnectionServiceFactory;
+use Catcoderphp\CustomConfigProvider\Factory\Service\Health\RedisConnectionServiceFactory;
+use Catcoderphp\CustomConfigProvider\Factory\Service\Health\SqlConnectionServiceFactory;
 use Catcoderphp\CustomConfigProvider\Mapper\Mapper;
 use Catcoderphp\CustomConfigProvider\Service\RequestHandlerService;
 use Catcoderphp\CustomConfigProvider\Service\ResponseHandlerService;
+use Catcoderphp\CustomConfigProvider\Service\Health\EndpointConnectionService;
+use Catcoderphp\CustomConfigProvider\Service\Health\MongoConnectionService;
+use Catcoderphp\CustomConfigProvider\Service\Health\RedisConnectionService;
+use Catcoderphp\CustomConfigProvider\Service\Health\SqlConnectionService;
 
 class ConfigProviderCollection
 {
@@ -19,6 +27,10 @@ class ConfigProviderCollection
         $this->dependencies = [
             ResponseHandlerService::class => ResponseHandlerServiceFactory::class,
             RequestHandlerService::class => RequestHandlerServiceFactory::class,
+            EndpointConnectionService::class => EndpointConnectionServiceFactory::class,
+            MongoConnectionService::class => MongoConnectionServiceFactory::class,
+            RedisConnectionService::class => RedisConnectionServiceFactory::class,
+            SqlConnectionService::class => SqlConnectionServiceFactory::class,
             Mapper::class => MapperFactory::class
         ];
     }
