1518b383c6f81ad3049c651b5cfe632fd0aede94
Hugo Hernández Valdez <hugo.hernandez@claroshop.com>
2021-05-26 17:53:32 -0500

    FIX: RedisConnectionServiceFactory error on not found Redis adapter

--- src/Factory/Service/Health/RedisConnectionServiceFactory.php
+++ src/Factory/Service/Health/RedisConnectionServiceFactory.php
@@ -4,16 +4,24 @@ namespace Catcoderphp\CustomConfigProvider\Factory\Service\Health;
 
 use Catcoderphp\CustomConfigProvider\Service\Health\RedisConnectionService;
 use Interop\Container\ContainerInterface;
+use Exception;
 
 class RedisConnectionServiceFactory
 {
     /**
      * @param ContainerInterface $container
      * @return RedisConnectionService
+     * @throws Exception
      */
     public function __invoke(ContainerInterface $container): RedisConnectionService
     {
-        $adapter = $container->get('Laminas\Cache\Storage\Adapter\Redis');
-        return new RedisConnectionService($adapter);
+        if ($container->has('Laminas\Cache\Storage\Adapter\Redis')) {
+            $adapter = $container->get('Laminas\Cache\Storage\Adapter\Redis');
+            return new RedisConnectionService($adapter);
+        }
+
+        throw new Exception(
+            'You do not have a previous configuration to be able to create an instance of RedisConnectionService'
+        );
     }
 }
